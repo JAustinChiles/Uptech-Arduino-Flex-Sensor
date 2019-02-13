4ea102ed2b34966f6579239c709e9716e1cee175
ARisingShadow <38085289+ARisingShadow@users.noreply.github.com>
2019-02-12 17:19:44 -0800

    Update sketch_nov19a.ino
    Put the declared variables in setup, turned hand open counter into a function

--- sketch_nov19a.ino
+++ sketch_nov19a.ino
@@ -3,16 +3,18 @@ Servo myServo;
 
 const int flexPin = A0; 
 const int ledPin = 7; 
-int closed = 0;
-int handOpenCount = 0;
-int maxOpen = 700;
-int minOpen = 890;
-int handClosed = 700;
-int handOpen = 890;
+
 void setup() 
 { 
   Serial.begin(9600);
-  pinMode(ledPin,OUTPUT);
+  pinMode(flexPin, INPUT);
+
+  int closed = 0;
+  int handOpenCount = 0;
+  int maxOpen = 700;
+  int minOpen = 890;
+  int handClosed = 700;
+  int handOpen = 890;
 } 
 
 void loop() 
@@ -25,35 +27,41 @@ void loop()
   Serial.println(flexValue);
 
 
-
+ 
   // Counts number of times hand has been open
-  if(flexValue<handClosed){
+  int handOpenCounter(int x, int y, int z){
+    int a
+  if(x<y){
     closed = 1;}
   if(closed=1){
-    if(flexValue>handOpen){
-      handOpenCount = handOpenCount + 1;
+    if(x>z){
+      a = a + 1;
       closed = 0;}
   }
-
+  return a;
+  }
+  
   //Keeps track of max hand length open
   if(flexValue>maxOpen){
     maxOpen = flexValue;
   }
   
   // Keeps track of farthest closed hand has been
-  if(flexvalue<minOpen){
+  if(flexValue<minOpen){
     minOpen = flexValue;
   }
+
+  handOpenCount = handOpenCounter(flexValue, handClosed, handOpen);
 
   //display hand opens
   Serial.print("Number of times hand was opened: ");
   Serial.println(handOpenCount);
 
   //LED light test
-  if(flexValue>890)
-     digitalWrite(ledPin,HIGH);
-  else
-    digitalWrite(ledPin,LOW);
+  //if(flexValue>890)
+  //   digitalWrite(ledPin,HIGH);
+  //else
+  //  digitalWrite(ledPin,LOW);
   
-  delay(20);
+  delay(1000);
 } 
