2461cd459e4357d8f3388101ca656640777d328f
ARisingShadow <38085289+ARisingShadow@users.noreply.github.com>
2019-03-07 12:51:02 -0800

    Angle and bug fix
    Fixed a bug where the hand open count would incorrectly increment before closing and added an angle tracker

--- sketch_nov19a.ino
+++ sketch_nov19a.ino
@@ -3,65 +3,79 @@ Servo myServo;
 
 const int flexPin = A0; 
 const int ledPin = 7; 
-
+bool closed = false;
+int handOpenCount = 0;
+int handClosed = 150;
+int handOpen = 250;
+double maxOpen = 0;
+double minOpen = 50;
+int angle = 0;
+double voltage = 0;
 void setup() 
 { 
   Serial.begin(9600);
-  pinMode(flexPin, INPUT);
-
-  int closed = 0;
-  int handOpenCount = 0;
-  int maxOpen = 700;
-  int minOpen = 890;
-  int handClosed = 700;
-  int handOpen = 890;
+  pinMode(ledPin,OUTPUT);
 } 
 
 void loop() 
 { 
   
   // Stores and displays the current flex value of sensor
-  int flexValue;  
+  double flexValue;  
   flexValue = analogRead(flexPin);
-  Serial.print("sensor: ");
-  Serial.println(flexValue);
+  voltage = (flexValue * 5 / 1023);
+  Serial.println(" ");
+  Serial.print("Voltage of sensor: ");  
+  Serial.println(voltage);
+
+  //calculates angle of sensor
+
+  angle = ((flexValue - 175 )/1.5);
 
 
- 
+
   // Counts number of times hand has been open
-  int handOpenCounter(int x, int y, int z){
-    int a
-  if(x<y){
-    closed = 1;}
-  if(closed=1){
-    if(x>z){
-      a = a + 1;
-      closed = 0;}
+  if(flexValue<handClosed){
+    closed = true;}
+  if(closed == true){
+    if(flexValue>handOpen){
+      handOpenCount = handOpenCount + 1;
+      closed = false;}
   }
-  return a;
-  }
-  
+
   //Keeps track of max hand length open
-  if(flexValue>maxOpen){
-    maxOpen = flexValue;
+  if(angle>maxOpen){
+    maxOpen = angle;
   }
   
   // Keeps track of farthest closed hand has been
-  if(flexValue<minOpen){
-    minOpen = flexValue;
+  if(angle<minOpen){
+    minOpen = angle;
   }
 
-  handOpenCount = handOpenCounter(flexValue, handClosed, handOpen);
+
+
+
+  //display angle of sensor
+  Serial.print("Angle of flex sensor: ");
+  Serial.print(angle);
+  Serial.println(" degrees");
 
   //display hand opens
   Serial.print("Number of times hand was opened: ");
   Serial.println(handOpenCount);
 
+  Serial.print("Max hand opened angle: ");
+  Serial.println(maxOpen);
+
+  Serial.print("Max hand closed angle: ");
+  Serial.println(minOpen);
+
   //LED light test
-  //if(flexValue>890)
+  //if(flexValu890)
   //   digitalWrite(ledPin,HIGH);
   //else
   //  digitalWrite(ledPin,LOW);
-  
-  delay(1000);
+ 
+  delay(200);
 } 
