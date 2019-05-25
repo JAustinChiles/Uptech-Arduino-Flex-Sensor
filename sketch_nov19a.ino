30f46c7db390def59f43bfebc39375538e340d0f
ARisingShadow <38085289+ARisingShadow@users.noreply.github.com>
2019-05-25 09:53:14 -0700

    Update sketch_nov19a.ino

--- sketch_nov19a.ino
+++ sketch_nov19a.ino
@@ -5,12 +5,14 @@ const int flexPin = A0;
 const int ledPin = 7; 
 bool closed = false;
 int handOpenCount = 0;
-int handClosed = 150;
-int handOpen = 250;
+int handClosed = 240;
+int handOpen = 170;
 double maxOpen = 0;
-double minOpen = 50;
+double minOpen = 0;
 int angle = 0;
+int tempAngle = 0;
 double voltage = 0;
+double Time = 0;
 void setup() 
 { 
   Serial.begin(9600);
@@ -19,26 +21,36 @@ void setup()
 
 void loop() 
 { 
+
+  Time = (Time + 0.2);
+  Serial.print(Time);
+  Serial.print(", ");
   
   // Stores and displays the current flex value of sensor
   double flexValue;  
   flexValue = analogRead(flexPin);
   voltage = (flexValue * 5 / 1023);
-  Serial.println(" ");
-  Serial.print("Voltage of sensor: ");  
-  Serial.println(voltage);
+  //Serial.println(" ");
+  //Serial.print("Voltage of sensor: ");  
+  Serial.print(voltage);
+  Serial.print(", ");
+  //Serial.print(flexValue);
+  //Serial.print(", ");
 
   //calculates angle of sensor
 
-  angle = ((flexValue - 175 )/1.5);
+  //angle = (((0.63172 * flexValue) - 105.28596 ));
+  //angle = ( 213.061 + ( -2.051 * flexValue) + (0.0025 * flexValue));
+  angle = ( 127.484 + ( -0.778 * flexValue) + (0.0122 * flexValue));
+  //angle = ( 121.165 + ( -1.139 * flexValue) + (0.0079 * flexValue));
 
 
 
   // Counts number of times hand has been open
-  if(flexValue<handClosed){
+  if(flexValue>handClosed){
     closed = true;}
   if(closed == true){
-    if(flexValue>handOpen){
+    if(flexValue<handOpen){
       handOpenCount = handOpenCount + 1;
       closed = false;}
   }
@@ -55,20 +67,24 @@ void loop()
 
 
 
+  //Serial.println(flexValue);
 
-  //display angle of sensor
-  Serial.print("Angle of flex sensor: ");
+
+  //display angle of sensor in degrees
+  //Serial.print("Angle of flex sensor: ");
   Serial.print(angle);
-  Serial.println(" degrees");
+  Serial.print(", ");
 
   //display hand opens
-  Serial.print("Number of times hand was opened: ");
-  Serial.println(handOpenCount);
+  //Serial.print("Number of times hand was opened: ");
+  Serial.print(handOpenCount);
+  Serial.print(", ");
 
-  Serial.print("Max hand opened angle: ");
-  Serial.println(maxOpen);
+  //Serial.print("Max hand opened angle: ");
+  Serial.print(maxOpen);
+  Serial.print(", ");
 
-  Serial.print("Max hand closed angle: ");
+  //Serial.print("Max hand closed angle: ");
   Serial.println(minOpen);
 
   //LED light test
